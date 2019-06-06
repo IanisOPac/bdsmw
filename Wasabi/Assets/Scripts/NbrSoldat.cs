0a2b34b3a769d24215ff1d7d36b52106c0a24d2a
Loic FABRE <fabreloi@iut-acy.local>
2019-06-06 17:59:39 +0200

    Full menu

---
+++ Wasabi/Assets/Scripts/NbrSoldat.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NbrSoldat : MonoBehaviour {
+
+    Text Text;
+    int nbr = 1;
+    string soldat = " soldat";
+    private void Start()
+    {
+        Text = GameObject.FindGameObjectWithTag("text").GetComponent<Text>();
+        Text.text = nbr.ToString() + soldat;
+    }
+     public void OnclickPlus ()
+    {
+        nbr++;
+        if (nbr > 1)
+        {
+            soldat = " soldats";
+        }
+        if (nbr > 5)
+        {
+            nbr = 5;
+        }
+        Text.text = nbr.ToString() + soldat;
+	}
+
+    public void OnclickMoins()
+    {
+        nbr--;
+        if (nbr > 1)
+        {
+            soldat = " soldats";
+        }
+        else if (nbr < 1)
+        {
+            nbr = 1;
+        }
+        else
+        {
+            soldat = " soldat";
+        }
+        Text.text = nbr.ToString() + soldat;
+    }
+}
---
+++ Wasabi/Assets/Scripts/ValeurVol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValeurVol : MonoBehaviour {
+
+    Text Valeur;
+
+    private void Start()
+    {
+        Valeur = GetComponent<Text>();
+    }
+
+    public void TextUpdate (float value)
+    {
+        Valeur.text = Mathf.RoundToInt(value * 100) + " ";
+	}
+}
